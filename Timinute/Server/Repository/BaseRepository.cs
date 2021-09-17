76e5680f55cfa5c84daa8734a95cd17422449555
Jan Mesarč <jame_581@windowslive.com>
2021-09-17 22:04:52 +0200

    Base repository implementation

---
+++ Timinute/Server/Repository/BaseRepository.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using Timinute.Server.Data;
+
+namespace Timinute.Server.Repository
+{
+    public class BaseRepository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        internal ApplicationDbContext context;
+        internal DbSet<TEntity> dbSet;
+        
+        public BaseRepository(ApplicationDbContext context)
+        {
+            this.context = context;
+            dbSet = context.Set<TEntity>();
+        }
+
+        public async Task Delete(TEntity entityToDelete)
+        {
+            if (context.Entry(entityToDelete).State == EntityState.Detached)
+            {
+                dbSet.Attach(entityToDelete);
+            }
+            dbSet.Remove(entityToDelete);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task Delete(object id)
+        {
+            TEntity entityToDelete = await dbSet.FindAsync(id);
+            await Delete(entityToDelete);
+        }
+
+        public async Task<IEnumerable<TEntity>> Get(Expression<Func<TEntity, bool>>? filter = null, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>>? orderBy = null, string includeProperties = "")
+        {
+            IQueryable<TEntity> query = dbSet;
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            if (!string.IsNullOrEmpty(includeProperties))
+            {
+                foreach (var includeProperty in includeProperties.Split
+                (new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProperty.Trim());
+                }
+            }
+
+            if (orderBy != null)
+            {
+                return await orderBy(query).ToListAsync();
+            }
+            else
+            {
+                return await query.ToListAsync();
+            }
+        }
+
+        public async Task<IEnumerable<TType>> Get<TType>(Expression<Func<TEntity, TType>> select, Expression<Func<TEntity, bool>>? where = null) where TType : class
+        {
+            if (where == null)
+            {
+                return await dbSet.Select(select).ToListAsync();
+            }
+
+            return await dbSet.Where(where).Select(select).ToListAsync();
+        }
+
+        public async Task<TEntity> GetByIdInclude(Expression<Func<TEntity, bool>>? filter = null, string includeProperties = "")
+        {
+            IQueryable<TEntity> query = dbSet;
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            if (!string.IsNullOrEmpty(includeProperties))
+            {
+                foreach (var includeProperty in includeProperties.Split
+                (new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProperty.Trim());
+                }
+            }
+            return await query.SingleOrDefaultAsync();
+        }
+
+        public async Task<TEntity> GetById(object id)
+        {
+            return await dbSet.FindAsync(id);
+        }
+        
+        public async Task<IEnumerable<TEntity>> GetWithRawSql(string query, params object[] parameters)
+        {
+            return await dbSet.FromSqlRaw(query, parameters).ToListAsync();
+        }
+
+        public async Task Insert(TEntity entity)
+        {
+            await dbSet.AddAsync(entity);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task Update(TEntity entityToUpdate)
+        {
+            context.Update(entityToUpdate);
+            await context.SaveChangesAsync();
+        }
+    }
+}
---
+++ Timinute/Server/Repository/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq.Expressions;
+
+namespace Timinute.Server.Repository
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        Task Delete(TEntity entityToDelete);
+        Task Delete(object id);
+        Task<IEnumerable<TEntity>> Get(Expression<Func<TEntity, bool>>? filter = null, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>>? orderBy = null, string includeProperties = "");
+        Task<IEnumerable<TType>> Get<TType>(Expression<Func<TEntity, TType>> select, Expression<Func<TEntity, bool>>? where = null) where TType : class;
+        Task<TEntity> GetByIdInclude(Expression<Func<TEntity, bool>>? filter = null, string includeProperties = "");
+        Task<TEntity> GetById(object id);
+        Task<IEnumerable<TEntity>> GetWithRawSql(string query, params object[] parameters);
+        Task Insert(TEntity entity);
+        Task Update(TEntity entityToUpdate);
+    }
+}
