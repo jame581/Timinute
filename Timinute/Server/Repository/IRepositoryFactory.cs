74c439a68d55866d792b0c7e3f7bb3643a85cabc
Jan Mesarč <jame_581@windowslive.com>
2021-09-17 22:05:02 +0200

    Repository factory and DI setup

---
+++ Timinute/Server/Repository/IRepositoryFactory.cs
@@ -0,0 +1,7 @@
+﻿namespace Timinute.Server.Repository
+{
+    public interface IRepositoryFactory
+    {
+        IRepository<TEntity> GetRepository<TEntity>() where TEntity : class, new();
+    }
+}
