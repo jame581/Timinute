c55789fd35c51d84553430bf213a2e097b60636d
Jan Mesarč <jame_581@windowslive.com>
2021-09-17 22:17:29 +0200

    Controller with basic method added

---
+++ Timinute/Server/Controllers/CompanyController.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Timinute.Server.Models;
+using Timinute.Server.Repository;
+using Timinute.Shared.Dtos;
+
+namespace Timinute.Server.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [ValidateAntiForgeryToken]
+    [Route("[controller]")]
+    public class CompanyController : ControllerBase
+    {
+        private readonly IRepository<Company> companyRepository;
+        private readonly IMapper mapper;
+
+        public CompanyController(IRepository<Company> companyRepository, IMapper mapper)
+        {
+            this.companyRepository = companyRepository;
+            this.mapper = mapper;
+        }
+
+        // GET: api/Categories
+        [HttpGet(Name = "GetAllCategories")]
+        public async Task<ActionResult<IEnumerable<CompanyDto>>> GetAllCompanies()
+        {
+            var companyList = await companyRepository.Get();
+            return Ok(mapper.Map<IEnumerable<CompanyDto>>(companyList));
+        }
+    }
+}
