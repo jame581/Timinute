55369bdd8fb2b368d01b61a2ce77a997b635cb3a
Jan Mesarč <jame_581@windowslive.com>
2021-09-18 18:49:08 +0200

    Migration for new roles

--- Timinute/Server/Data/Migrations/20210918164604_ApplicationRoleValues.Designer.cs
+++ Timinute/Server/Data/Migrations/20210918164604_ApplicationRoleValues.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Timinute.Server.Data;
 
@@ -11,9 +12,10 @@ using Timinute.Server.Data;
 namespace Timinute.Server.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210918164604_ApplicationRoleValues")]
+    partial class ApplicationRoleValues
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
