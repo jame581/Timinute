5611745764bbb48f706ecf945246dcba44966735
Jan Mesarč <jame_581@windowslive.com>
2021-09-22 21:33:46 +0200

    Basic page with basic table

---
+++ Timinute/Client/Models/TrackedTask.cs
@@ -0,0 +1,31 @@
+﻿using Timinute.Shared.Dtos.TrackedTask;
+
+namespace Timinute.Client.Models
+{
+    public class TrackedTask
+    {
+        public string TaskId { get; set; } = null!;
+        public string Name { get; set; } = null!;
+        public TimeSpan Duration { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string? ProjectId { get; set; }
+        public string UserId { get; set; } = null!;
+
+        public TrackedTask()
+        {
+
+        }
+
+        public TrackedTask(TrackedTaskDto trackedTask)
+        {
+            TaskId = trackedTask.TaskId;
+            Name = trackedTask.Name;
+            Duration = trackedTask.Duration;
+            StartDate = trackedTask.StartDate;
+            EndDate = trackedTask.EndDate;
+            ProjectId = trackedTask.ProjectId;
+            UserId = trackedTask.UserId;
+        }
+    }
+}
---
+++ Timinute/Client/Pages/TrackedTasks/TrackedTasks.razor.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Timinute.Client.Models;
+
+namespace Timinute.Client.Pages.TrackedTasks
+{
+    public partial class TrackedTasks
+    {
+        public readonly IList<TrackedTask> TrackedTasksList = new List<TrackedTask>();
+
+        [CascadingParameter]
+        private Task<AuthenticationState> authenticationStateTask { get; set; }
+
+        [Inject]
+        protected NavigationManager Navigation { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            var authState = await authenticationStateTask;
+            var user = authState.User;
+
+            if (!user.Identity.IsAuthenticated)
+                Navigation.NavigateTo($"{Navigation.BaseUri}auth/login", true);
+
+            TrackedTasksList.Add(new TrackedTask { TaskId = "1", Name = "Task 1", Duration = TimeSpan.FromMinutes(120), StartDate = DateTime.Now, EndDate = DateTime.Now.AddMinutes(120) });
+            TrackedTasksList.Add(new TrackedTask { TaskId = "2", Name = "Task 2", Duration = TimeSpan.FromMinutes(60), StartDate = DateTime.Now, EndDate = DateTime.Now.AddMinutes(60) });
+            TrackedTasksList.Add(new TrackedTask { TaskId = "3", Name = "Task 3", Duration = TimeSpan.FromMinutes(180), StartDate = DateTime.Now, EndDate = DateTime.Now.AddMinutes(180) });
+
+           // ApiPath = $"{Navigation.BaseUri}api/ApplicationUserRead";
+        }
+    }
+}
