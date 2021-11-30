73fcba70728dc8df813d710dc15e5aceedf655cb
Jan Mesarč <jame_581@windowslive.com>
2021-11-30 18:19:00 +0100

    Time tracker component basic look

--- Timinute/Client/Components/TimeTracker.razor.cs
+++ Timinute/Client/Components/TimeTracker.razor.cs
@@ -1,6 +1,13 @@
-﻿namespace Timinute.Client.Components
+﻿using Timinute.Shared.Dtos.TrackedTask;
+namespace Timinute.Client.Components
 {
     public partial class TimeTracker
     {
+        private CreateTrackedTaskDto trackedTask = new();
+
+        private void HandleValidSubmit()
+        {
+            //handle valid submit            
+        }
     }
 }
