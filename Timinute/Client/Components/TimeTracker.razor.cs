5d390755b3fe0f2d42dec3cfcadfb00380edd492
Jan Mesarč <jame_581@windowslive.com>
2021-12-05 17:39:56 +0100

    Time tracker component basic functionality

--- Timinute/Client/Components/TimeTracker.razor.cs
+++ Timinute/Client/Components/TimeTracker.razor.cs
@@ -1,13 +1,128 @@
-﻿using Timinute.Shared.Dtos.TrackedTask;
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using System.Net.Http.Json;
+using System.Text.Json;
+using Timinute.Client.Helpers;
+using Timinute.Shared.Dtos.Project;
+using Timinute.Shared.Dtos.TrackedTask;
+
 namespace Timinute.Client.Components
 {
     public partial class TimeTracker
     {
-        private CreateTrackedTaskDto trackedTask = new();
+        private TrackedTaskDto trackedTask = new();
 
-        private void HandleValidSubmit()
+        private EditContext editContext { get; set; }
+
+        private bool stopWatchRunning = false;
+
+        private string durationProxy { get; set; } = "00:00:00";
+
+        private string exceptionMessage { get; set; }
+
+        [Inject]
+        private IHttpClientFactory clientFactory { get; set; }
+
+        protected override void OnInitialized()
         {
-            //handle valid submit            
+            editContext = new(trackedTask);
+            editContext.EnableDataAnnotationsValidation();
+        }
+
+        private async Task HandleValidSubmit()
+        {
+            if (editContext != null && editContext.Validate())
+            {
+
+                if (stopWatchRunning)
+                {
+                    await StopWatch();
+                }
+                else
+                {
+                    await StartWatch();
+                }
+            }           
+        }
+
+        private async Task StartWatch()
+        {
+            trackedTask.StartDate = DateTime.Now;
+            trackedTask.Duration = new TimeSpan();
+
+            var client = clientFactory.CreateClient(Constants.API.ClientName);
+
+            CreateTrackedTaskDto createTrackedTaskDto = new()
+            {
+                Name = trackedTask.Name,
+                StartDate = trackedTask.StartDate
+            };
+
+            try
+            {
+                var responseMessage = await client.PostAsJsonAsync(Constants.API.TrackedTask.Create, createTrackedTaskDto);
+                responseMessage.EnsureSuccessStatusCode();
+
+                var trackedTaskDto = await responseMessage.Content.ReadFromJsonAsync<TrackedTaskDto>();//.ReadAsStringAsync();
+
+                if (trackedTaskDto != null)
+                {
+                    trackedTask.TaskId = trackedTaskDto.TaskId;
+                    stopWatchRunning = true;
+                }
+                //if (!string.IsNullOrEmpty(response))
+                //{
+                //    TrackedTaskDto trackedTaskDto = JsonSerializer.Deserialize<TrackedTaskDto>(response);
+                //}
+
+            }
+            catch (Exception ex)
+            {
+                exceptionMessage = ex.Message;
+            }
+
+            while (stopWatchRunning)
+            {
+                await Task.Delay(1000);
+
+                if (stopWatchRunning)
+                {
+                    trackedTask.Duration = trackedTask.Duration.Add(TimeSpan.FromSeconds(1));
+                    durationProxy = trackedTask.Duration.ToString();
+                    StateHasChanged();
+                }
+            }
+        }
+
+        private async Task StopWatch()
+        {
+            var client = clientFactory.CreateClient(Constants.API.ClientName);
+
+            UpdateTrackedTaskDto updateTrackedTaskDto = new()
+            {
+                TaskId = trackedTask.TaskId,
+                Name = trackedTask.Name,
+                Project = trackedTask.Project,
+                ProjectId = trackedTask.ProjectId,
+                StartDate = trackedTask.StartDate,
+                EndDate = trackedTask.StartDate + trackedTask.Duration             
+            };
+
+            try
+            {
+                var responseMessage = await client.PutAsJsonAsync(Constants.API.TrackedTask.Update, updateTrackedTaskDto);
+                responseMessage.EnsureSuccessStatusCode();    
+                
+                trackedTask = new();
+                stopWatchRunning = false;
+                durationProxy = "00:00:00";
+
+                StateHasChanged();
+            }
+            catch (Exception ex)
+            {
+                exceptionMessage = ex.Message;
+            }
         }
     }
 }
