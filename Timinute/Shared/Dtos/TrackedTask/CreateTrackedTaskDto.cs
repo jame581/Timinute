50afb2cb9a56e892a33d9335e00e2f624219690e
Jan Mesarč <jame_581@windowslive.com>
2021-12-05 17:39:01 +0100

    Create tracked task annotations added

--- Timinute/Shared/Dtos/TrackedTask/CreateTrackedTaskDto.cs
+++ Timinute/Shared/Dtos/TrackedTask/CreateTrackedTaskDto.cs
@@ -1,13 +1,21 @@
-﻿using Timinute.Shared.Dtos.Project;
+﻿using System.ComponentModel.DataAnnotations;
+using Timinute.Shared.Dtos.Project;
 
 namespace Timinute.Shared.Dtos.TrackedTask
 {
     public class CreateTrackedTaskDto
     {
+        [Required]
+        [StringLength(50, ErrorMessage = "Name of project is too long.", MinimumLength = 2)]
         public string Name { get; set; } = null!;
+
+        [Required]
         public DateTime StartDate { get; set; }
+        
         public TimeSpan Duration { get; set; }
+        
         public string? ProjectId { get; set; }
+        
         public ProjectDto? Project { get; set; }
     }
 }
