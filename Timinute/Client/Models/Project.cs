f624018ac1f381086eef95c634dda97e3915fd4b
Jan Mesarč <jame_581@windowslive.com>
2022-01-05 19:40:51 +0100

    Model for project added

---
+++ Timinute/Client/Models/Project.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Timinute.Shared.Dtos.Project;
+
+namespace Timinute.Client.Models
+{
+    public class Project
+    {
+        public string ProjectId { get; set; } = null!;
+
+        [Required]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Name can not have less then 3 characters and more then 50.")]
+        public string Name { get; set; } = null!;
+
+        public string? CompanyId { get; set; }
+
+        public Project()
+        {
+
+        }
+
+        public Project(ProjectDto project)
+        {
+            ProjectId = project.ProjectId;
+            Name = project.Name;
+            CompanyId = project.CompanyId;
+        }
+    }
+}
