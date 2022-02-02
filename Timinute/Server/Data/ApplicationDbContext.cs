08dbdd0a66d1215b82fb39593cbb6d81fcc052ff
Jan Mesarč <jame_581@windowslive.com>
2022-02-02 13:05:27 +0100

    Testing data added

--- Timinute/Server/Data/ApplicationDbContext.cs
+++ Timinute/Server/Data/ApplicationDbContext.cs
@@ -9,6 +9,8 @@ namespace Timinute.Server.Data
 {
     public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
     {
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<ApplicationRole> ApplicationRoles { get; set; }
         public DbSet<Company> Companies { get; set; } = null!;
         public DbSet<TrackedTask> TrackedTasks { get; set; } = null!;
         public DbSet<Project> Projects { get; set; } = null!;
@@ -92,7 +94,28 @@ namespace Timinute.Server.Data
                 new ApplicationRole{Name = Constants.Roles.Admin, NormalizedName =  Constants.Roles.Admin.ToUpper(), Description = "Admin role with highest rights."}
             };
 
+            var applicationUsers = new List<ApplicationUser>()
+            {
+                new ApplicationUser { Id = Guid.NewGuid().ToString(), Email = "test1@email.com", FirstName = "Jan", LastName = "Testovic", EmailConfirmed = true, UserName = "test1@email.com", PasswordHash = "AQAAAAEAACcQAAAAEDgV3QGcSGxXfgIEFYvljstwmQb05lu59FQY/6H4R7SLAZkYc2uJCmNyio51dtfuGg=="},
+                new ApplicationUser { Id = Guid.NewGuid().ToString(), Email = "test2@email.com", FirstName = "Ivana", LastName = "Maricenkova", EmailConfirmed = true, UserName = "test2@email.com", PasswordHash = "AQAAAAEAACcQAAAAEDgV3QGcSGxXfgIEFYvljstwmQb05lu59FQY/6H4R7SLAZkYc2uJCmNyio51dtfuGg=="},
+                new ApplicationUser { Id = Guid.NewGuid().ToString(), Email = "test3@email.com", FirstName = "Marek", LastName = "Klukac", EmailConfirmed = true, UserName = "test3@email.com", PasswordHash = "AQAAAAEAACcQAAAAEDgV3QGcSGxXfgIEFYvljstwmQb05lu59FQY/6H4R7SLAZkYc2uJCmNyio51dtfuGg=="},
+            };
+
             builder.Entity<ApplicationRole>().HasData(roles);
+            builder.Entity<ApplicationUser>().HasData(applicationUsers);
+
+            var trackedTasks = new List<TrackedTask>
+            {
+                new TrackedTask { TaskId = Guid.NewGuid().ToString(), Name = "Project A", Duration = TimeSpan.FromHours(2), StartDate = new DateTime(2022, 1, 1, 9, 0, 0), EndDate = new DateTime(2022, 1, 1, 11, 0, 0), UserId = applicationUsers[0].Id },
+                new TrackedTask { TaskId = Guid.NewGuid().ToString(), Name = "Project B", Duration = TimeSpan.FromHours(3), StartDate = new DateTime(2022, 2, 2, 10, 0, 0), EndDate = new DateTime(2022, 2, 2, 13, 0, 0), UserId = applicationUsers[0].Id },
+                new TrackedTask { TaskId = Guid.NewGuid().ToString(), Name = "Project C", Duration = TimeSpan.FromHours(4), StartDate = new DateTime(2022, 1, 1, 11, 0, 0), EndDate = new DateTime(2022, 1, 1, 15, 0, 0), UserId = applicationUsers[0].Id },
+                new TrackedTask { TaskId = Guid.NewGuid().ToString(), Name = "Project D", Duration = TimeSpan.FromHours(5), StartDate = new DateTime(2022, 2, 2, 12, 0, 0), EndDate = new DateTime(2022, 2, 2, 17, 0, 0), UserId = applicationUsers[1].Id },
+                new TrackedTask { TaskId = Guid.NewGuid().ToString(), Name = "Project E", Duration = TimeSpan.FromHours(6), StartDate = new DateTime(2022, 1, 1, 13, 0, 0), EndDate = new DateTime(2022, 1, 1, 19, 0, 0), UserId = applicationUsers[1].Id },
+                new TrackedTask { TaskId = Guid.NewGuid().ToString(), Name = "Project F", Duration = TimeSpan.FromHours(7), StartDate = new DateTime(2022, 2, 2, 14, 0, 0), EndDate = new DateTime(2022, 2, 2, 21, 0, 0), UserId = applicationUsers[2].Id },
+                new TrackedTask { TaskId = Guid.NewGuid().ToString(), Name = "Project G", Duration = TimeSpan.FromHours(7), StartDate = new DateTime(2022, 2, 2, 14, 0, 0), EndDate = new DateTime(2022, 2, 2, 21, 0, 0), UserId = applicationUsers[2].Id },
+            };
+
+            builder.Entity<TrackedTask>().HasData(trackedTasks);
         }
     }
 }
--- Timinute/Server/Helpers/Constants.cs
+++ Timinute/Server/Helpers/Constants.cs
@@ -11,7 +11,10 @@
         public static class Claims
         {
             public const string UserId = "sub";
-            
+            public const string LastLogin = "LastLogin";
+            public const string Fullname = "FullName";
+            public const string Role = "Role";
+
         }
     }
 }
