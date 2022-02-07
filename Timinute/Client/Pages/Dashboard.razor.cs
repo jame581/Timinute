866944967eebe1ee5c95325f3f9085820fb5cc80
Jan Mesarč <jame_581@windowslive.com>
2022-02-07 19:40:25 +0100

    Nav menu edited and empty dashboard page added

---
+++ Timinute/Client/Pages/Dashboard.razor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace Timinute.Client.Pages
+{
+    public partial class Dashboard
+    {
+        [CascadingParameter]
+        private Task<AuthenticationState> AuthenticationStateTask { get; set; } = null!;
+
+        [Inject]
+        protected NavigationManager Navigation { get; set; } = null!;
+
+        protected override async Task OnInitializedAsync()
+        {
+            var authState = await AuthenticationStateTask;
+            var user = authState.User;
+
+            if (user.Identity != null && !user.Identity.IsAuthenticated)
+                Navigation.NavigateTo($"{Navigation.BaseUri}auth/login", true);
+        }
+    }
+}
