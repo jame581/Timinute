8f8e1e667dbf1ab6a67749215bdb2719b0e15600
Jan Mesarč <jame_581@windowslive.com>
2022-02-10 17:23:27 +0100

    Radzen added to project

---
+++ Timinute/Client/Models/Dashboard/ProjectDataItem.cs
@@ -0,0 +1,15 @@
+﻿namespace Timinute.Client.Models.Dashboard
+{
+    public class ProjectDataItem
+    {
+        public string Project { get; set; }
+        public TimeSpan Time { get; set; }
+        public double TimeInt { get => Time.TotalSeconds; }
+
+        public ProjectDataItem(string project, TimeSpan time)
+        {
+            Project = project;
+            Time = time;
+        }
+    }
+}
--- Timinute/Client/Program.cs
+++ Timinute/Client/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Timinute.Client;
+using Radzen;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -15,6 +16,8 @@ builder.Services.AddBlazoredSessionStorage();
 
 // Supply HttpClient instances that include access tokens when making requests to the server project
 builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("Timinute.ServerAPI"));
+
+builder.Services.AddScoped<NotificationService>();
 
 builder.Services.AddApiAuthorization();
 
