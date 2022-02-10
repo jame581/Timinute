fe2cf145bdbf55d5058dd5548d6dc554e80f3ccd
Jan Mesarč <jame_581@windowslive.com>
2022-02-10 21:27:37 +0100

    Column chart added

---
+++ Timinute/Client/Components/Dashboard/ProjectColumnChart.razor.cs
@@ -0,0 +1,37 @@
+﻿using Timinute.Client.Models.Dashboard;
+
+namespace Timinute.Client.Components.Dashboard
+{
+    public partial class ProjectColumnChart
+    {
+        ProjectDataItem[] projectTimeMonth1 = new ProjectDataItem[] {
+            new ProjectDataItem("Project 1", TimeSpan.FromMinutes(60)),
+            new ProjectDataItem("Project 2", TimeSpan.FromMinutes(120)),
+            new ProjectDataItem("Project 3", TimeSpan.FromMinutes(240)),
+            new ProjectDataItem("Project 4", TimeSpan.FromMinutes(275)),
+            new ProjectDataItem("Project 5", TimeSpan.FromMinutes(150)),
+        };
+
+        ProjectDataItem[] projectTimeMonth2 = new ProjectDataItem[] {
+            new ProjectDataItem("Project 1", TimeSpan.FromMinutes(60)),
+            new ProjectDataItem("Project 2", TimeSpan.FromMinutes(120)),
+            new ProjectDataItem("Project 3", TimeSpan.FromMinutes(240)),
+            new ProjectDataItem("Project 4", TimeSpan.FromMinutes(275)),
+            new ProjectDataItem("Project 5", TimeSpan.FromMinutes(150)),
+        };
+
+        ProjectDataItem[] projectTimeMonth3 = new ProjectDataItem[] {
+            new ProjectDataItem("Project 1", TimeSpan.FromMinutes(60)),
+            new ProjectDataItem("Project 2", TimeSpan.FromMinutes(120)),
+            new ProjectDataItem("Project 3", TimeSpan.FromMinutes(240)),
+            new ProjectDataItem("Project 4", TimeSpan.FromMinutes(275)),
+            new ProjectDataItem("Project 5", TimeSpan.FromMinutes(150)),
+        };
+
+        string FormatAsTime(object value)
+        {
+            TimeSpan timeSpan = TimeSpan.FromSeconds((double)value);
+            return timeSpan.ToString(@"hh\:mm\:ss");
+        }
+    }
+}
