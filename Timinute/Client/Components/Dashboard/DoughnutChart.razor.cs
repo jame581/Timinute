81175030f5880f542b95089a241e18cd4336ca35
Jan Mesarč <jame_581@windowslive.com>
2022-02-10 21:01:25 +0100

    Dashboard doughnut chart added

---
+++ Timinute/Client/Components/Dashboard/DoughnutChart.razor.cs
@@ -0,0 +1,14 @@
+﻿using Timinute.Client.Models.Dashboard;
+namespace Timinute.Client.Components.Dashboard
+{
+    public partial class DoughnutChart
+    {
+        ProjectDataItem[] projectTime = new ProjectDataItem[] {
+            new ProjectDataItem("Project 1", TimeSpan.FromMinutes(60)),
+            new ProjectDataItem("Project 2", TimeSpan.FromMinutes(120)),
+            new ProjectDataItem("Project 3", TimeSpan.FromMinutes(240)),
+            new ProjectDataItem("Project 4", TimeSpan.FromMinutes(275)),
+            new ProjectDataItem("Project 5", TimeSpan.FromMinutes(150)),
+        };
+    }
+}
