8dadbe90bed2e43aec416bda9a53a495ca570f27
Jan Mesarč <jame_581@windowslive.com>
2022-02-11 18:52:31 +0100

    Add treacked task component notification added

--- Timinute/Client/Components/TrackedTasks/AddTrackedTask.razor.cs
+++ Timinute/Client/Components/TrackedTasks/AddTrackedTask.razor.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Forms;
+using Radzen;
 using System.Net.Http.Json;
 using Timinute.Client.Helpers;
 using Timinute.Client.Models;
@@ -10,8 +11,6 @@ namespace Timinute.Client.Components.TrackedTasks
     public partial class AddTrackedTask
     {
         private TrackedTask NewTrackedTask { get; set; } = new() { StartDate = DateTime.Now };
-
-        private string exceptionMessage = "";
 
         bool displayValidationErrorMessages = false;
 
@@ -31,6 +30,9 @@ namespace Timinute.Client.Components.TrackedTasks
         [Inject]
         private IHttpClientFactory ClientFactory { get; set; } = null!;
 
+        [Inject]
+        private NotificationService notificationService { get; set; } = null!;
+
         private async Task HandleValidSubmit()
         {
             var client = ClientFactory.CreateClient(Constants.API.ClientName);
@@ -48,17 +50,21 @@ namespace Timinute.Client.Components.TrackedTasks
                 responseMessage.EnsureSuccessStatusCode();
 
                 displayValidationErrorMessages = false;
+                notificationService.Notify(NotificationSeverity.Success, "Success", "Tracked task saved", 3000);
             }
             catch (Exception ex)
             {
-                exceptionMessage = ex.Message;
-                displayValidationErrorMessages = true;
+                notificationService.Notify(NotificationSeverity.Error, "Something happend", ex.Message, 5000);
             }
         }
 
         private void HandleInvalidSubmit(EditContext context)
         {
-            displayValidationErrorMessages = true;
+            var errorMessages = context.GetValidationMessages();
+            foreach (var errorMessage in errorMessages)
+            {
+                notificationService.Notify(NotificationSeverity.Error, "Validation error", errorMessage, 5000);
+            }
         }
     }
 }
