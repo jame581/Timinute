bcd9ff0d044c664d3cae91cf50b1dcc658d09a4d
Jan Mesarč <jame_581@windowslive.com>
2022-02-11 18:12:20 +0100

    Dtos added

--- Timinute/Client/Models/Dashboard/ProjectDataItem.cs
+++ Timinute/Client/Models/Dashboard/ProjectDataItem.cs
@@ -5,13 +5,15 @@
         public string ProjectId { get; set; }
         public string ProjectName { get; set; }
         public TimeSpan Time { get; set; }
+        public string YearAndMonth { get; set; }
         public double TimeInt { get => Time.TotalSeconds; }
 
-        public ProjectDataItem(string projectId, string projectName, TimeSpan time)
+        public ProjectDataItem(string projectId, string projectName, TimeSpan time, string yearAndMonth)
         {
             ProjectId = projectId;
             ProjectName = projectName;
             Time = time;
+            YearAndMonth = yearAndMonth;
         }
     }
 }
---
+++ Timinute/Shared/Dtos/Dashboard/AmountOfWorkTimeDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Timinute.Shared.Dtos.Dashboard
+{
+    public class AmountOfWorkTimeDto
+    {
+        public string AmountWorkTimeText { get; set; }
+        public double AmountWorkTime { get; set; }
+
+        public string TopProject { get; set; }
+        public string TopProjectAmounTimeText { get; set; }
+        public double TopProjectAmounTime { get; set; }
+    }
+}
