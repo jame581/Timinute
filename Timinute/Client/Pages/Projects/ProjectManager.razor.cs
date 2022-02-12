a5cca612dfab591d657730014d5e35c5fb823d52
Jan Mesarč <jame_581@windowslive.com>
2022-02-12 14:45:52 +0100

    Project manager using Radzen table

--- Timinute/Client/Pages/Projects/ProjectManager.razor.cs
+++ Timinute/Client/Pages/Projects/ProjectManager.razor.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
 using Radzen;
+using Radzen.Blazor;
 using System.Net.Http.Json;
 using Timinute.Client.Helpers;
 using Timinute.Client.Models;
@@ -11,6 +12,12 @@ namespace Timinute.Client.Pages.Projects
     public partial class ProjectManager
     {
         private List<Project> ProjectsList { get; set; } = new();
+
+        private int projectCount = 0;
+
+        private bool isLoading = true;
+
+        private RadzenDataGrid<Project> radzenDataGrid = null!;
 
         [CascadingParameter]
         private Task<AuthenticationState> AuthenticationStateTask { get; set; } = null!;
@@ -37,12 +44,13 @@ namespace Timinute.Client.Pages.Projects
                 Navigation.NavigateTo($"{Navigation.BaseUri}auth/login", true);
 
             await RefreshTable();
+            radzenDataGrid.Reload();
         }
 
         private async Task RefreshTable()
         {
             var client = ClientFactory.CreateClient(Constants.API.ClientName);
-
+            isLoading = true;
             try
             {
                 var response = await client.GetFromJsonAsync<ProjectDto[]>(Constants.API.Project.GetAll);
@@ -53,14 +61,78 @@ namespace Timinute.Client.Pages.Projects
 
                     foreach (var item in response)
                         ProjectsList.Add(new Project(item));
-                }
 
-                StateHasChanged();
+                    projectCount = ProjectsList.Count;
+                }
             }
             catch (Exception ex)
             {
                 notificationService.Notify(NotificationSeverity.Error, "Validation error", ex.Message, 5000);
             }
+
+            isLoading = false;
+            StateHasChanged();
+        }
+
+        //async Task LoadData(LoadDataArgs args)
+        //{
+        //    // This demo is using https://dynamic-linq.net
+        //    //var query = dbContext.Customers.AsQueryable();
+
+        //    //if (!string.IsNullOrEmpty(args.Filter))
+        //    //{
+        //    //    // Filter via the Where method
+        //    //    query = query.Where(args.Filter);
+        //    //}
+
+        //    //if (!string.IsNullOrEmpty(args.OrderBy))
+        //    //{
+        //    //    // Sort via the OrderBy method
+        //    //    query = query.OrderBy(args.OrderBy);
+        //    //}
+
+        //    await RefreshTable();
+
+        //    // Important!!! Make sure the Count property of RadzenDataGrid is set.
+        //    //projectCount = query.Count();
+
+        //    //// Perform paging via Skip and Take.
+        //    //customers = query.Skip(args.Skip.Value).Take(args.Top.Value).ToList();
+
+        //    // Add StateHasChanged(); for DataGrid to update if your LoadData method is async.
+        //    StateHasChanged();
+        //}
+
+        async Task LoadData(LoadDataArgs args)
+        {
+            isLoading = true;
+
+            //await Task.Yield();
+
+            // This demo is using https://dynamic-linq.net
+            //var query = dbContext.Employees.AsQueryable();
+
+            //if (!string.IsNullOrEmpty(args.Filter))
+            //{
+            //    // Filter via the Where method
+            //    query = query.Where(args.Filter);
+            //}
+
+            //if (!string.IsNullOrEmpty(args.OrderBy))
+            //{
+            //    // Sort via the OrderBy method
+            //    query = query.OrderBy(args.OrderBy);
+            //}
+
+            // Important!!! Make sure the Count property of RadzenDataGrid is set.
+            //projectCount = query.Count();
+
+            // Perform paginv via Skip and Take.
+            //= query.Skip(args.Skip.Value).Take(args.Top.Value).ToList();
+
+            await RefreshTable();
+
+            isLoading = false;
         }
 
         private async Task HandleProjectAdded(Project project)
