4dba9b8900c10f2244fe8277de11c52663fcf617
Jan Mesarč <jame_581@windowslive.com>
2022-02-15 16:11:09 +0100

    Program.cs polished

--- Timinute/Server/Controllers/OidcConfigurationController.cs
+++ Timinute/Server/Controllers/OidcConfigurationController.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Timinute.Server.Controllers
 {
+    [AllowAnonymous]
     public class OidcConfigurationController : Controller
     {
         private readonly ILogger<OidcConfigurationController> _logger;
--- Timinute/Server/Program.cs
+++ Timinute/Server/Program.cs
@@ -2,6 +2,7 @@ using AutoMapper;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 using System.Security.Claims;
@@ -21,44 +22,8 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddIdentity<ApplicationUser, ApplicationRole>(options =>
-    {
-        options.SignIn.RequireConfirmedAccount = true;
-
-        options.Password.RequiredLength = 8;
-        options.Password.RequiredUniqueChars = 4;
-        options.Password.RequireDigit = true;
-        options.Password.RequireLowercase = true;
-        options.Password.RequireNonAlphanumeric = false;
-        options.Password.RequireUppercase = true;
-
-        options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
-        options.Lockout.MaxFailedAccessAttempts = 5;
-        options.Lockout.AllowedForNewUsers = true;
-
-    })
-    .AddEntityFrameworkStores<ApplicationDbContext>()
-    .AddRoles<ApplicationRole>()
-    .AddDefaultTokenProviders()
-    .AddDefaultUI()
-    .AddSignInManager<AppSingInManager>()
-    .AddClaimsPrincipalFactory<ApplicationUserClaimsPrincipalFactory>();
-
-builder.Services.Configure<IdentityOptions>(options =>
-{
-    options.ClaimsIdentity.UserIdClaimType = ClaimTypes.NameIdentifier;
-    options.ClaimsIdentity.UserNameClaimType = ClaimTypes.Name;
-    options.ClaimsIdentity.RoleClaimType = ClaimTypes.Role;
-});
-
-builder.Services.AddIdentityServer()
-    .AddApiAuthorization<ApplicationUser, ApplicationDbContext>(options =>
-    {
-        options.IdentityResources["openid"].UserClaims.Add(Constants.Claims.Fullname);
-        options.IdentityResources["openid"].UserClaims.Add(Constants.Claims.LastLogin);
-        options.IdentityResources["openid"].UserClaims.Add(Constants.Claims.Role);
-    })
-    .AddJwtBearerClientAuthentication();
+// Identity configuration
+IdentitySetup();
 
 builder.Services.AddAuthentication()
     .AddIdentityServerJwt();
@@ -66,42 +31,46 @@ builder.Services.AddAuthentication()
 builder.Logging.AddConsole();
 
 builder.Services.AddRazorPages();
+
 builder.Services.AddResponseCaching();
+
 builder.Services.AddControllers(options =>
 {
-    options.CacheProfiles.Add("Default120",
-        new CacheProfile()
+    options.ReturnHttpNotAcceptable = true;
+    options.Filters.Add(new AuthorizeFilter());
+    options.CacheProfiles.Add("Default120", new CacheProfile() { Duration = 120, Location = ResponseCacheLocation.Client });
+})
+    .AddXmlDataContractSerializerFormatters()
+    .ConfigureApiBehaviorOptions(options =>
+    {
+        options.InvalidModelStateResponseFactory = context =>
         {
-            Duration = 120
-        });
-});
+            var problemDetails = new ValidationProblemDetails(context.ModelState)
+            {
+                Type = "https://timinute.azurewebsites.net/modelvalidationproblem",
+                Title = "One or more model validation errors occurred.",
+                Status = StatusCodes.Status422UnprocessableEntity,
+                Detail = "See the errors property for details.",
+                Instance = context.HttpContext.Request.Path
+            };
 
-// DI
-builder.Services.AddScoped<IUserClaimsPrincipalFactory<ApplicationUser>, ApplicationUserClaimsPrincipalFactory>();
-builder.Services.AddTransient<IRepositoryFactory, RepositoryFactory>();
+            problemDetails.Extensions.Add("traceId", context.HttpContext.TraceIdentifier);
+
+            return new UnprocessableEntityObjectResult(problemDetails)
+            {
+                ContentTypes = { "application/problem+json" }
+            };
+        };
+    });
+
+// DI Configuration
+DependecyInjection();
 
 // Auto Mapper Configurations
-var mappingConfig = new MapperConfiguration(mc =>
-{
-    mc.AddProfile(new MappingProfile());
-});
+AutoMapperConfiguration();
 
-builder.Services.AddSingleton(mappingConfig.CreateMapper());
-
-// Swagger setup
-builder.Services.AddEndpointsApiExplorer();
-
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo
-    {
-        Title = "Timinute API",
-        Description = "Docs for Timinute API",
-        Version = "v1",
-        Contact = new OpenApiContact { Email = "jame_581@windowslive.com", Name = "Jan Mesarc" }
-    });
-    c.ResolveConflictingActions(apiDescriptors => apiDescriptors.First());
-});
+// Swagger Configuration
+SwaggerSetup();
 
 var app = builder.Build();
 
@@ -146,3 +115,79 @@ app.MapControllers();
 app.MapFallbackToFile("index.html");
 
 app.Run();
+
+void IdentitySetup()
+{
+    builder.Services.AddIdentity<ApplicationUser, ApplicationRole>(options =>
+    {
+        options.SignIn.RequireConfirmedAccount = true;
+
+        options.Password.RequiredLength = 8;
+        options.Password.RequiredUniqueChars = 4;
+        options.Password.RequireDigit = true;
+        options.Password.RequireLowercase = true;
+        options.Password.RequireNonAlphanumeric = false;
+        options.Password.RequireUppercase = true;
+
+        options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+        options.Lockout.MaxFailedAccessAttempts = 5;
+        options.Lockout.AllowedForNewUsers = true;
+
+    })
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddRoles<ApplicationRole>()
+    .AddDefaultTokenProviders()
+    .AddDefaultUI()
+    .AddSignInManager<AppSingInManager>()
+    .AddClaimsPrincipalFactory<ApplicationUserClaimsPrincipalFactory>();
+
+    builder.Services.Configure<IdentityOptions>(options =>
+    {
+        options.ClaimsIdentity.UserIdClaimType = ClaimTypes.NameIdentifier;
+        options.ClaimsIdentity.UserNameClaimType = ClaimTypes.Name;
+        options.ClaimsIdentity.RoleClaimType = ClaimTypes.Role;
+    });
+
+    builder.Services.AddIdentityServer()
+        .AddApiAuthorization<ApplicationUser, ApplicationDbContext>(options =>
+        {
+            options.IdentityResources["openid"].UserClaims.Add(Constants.Claims.Fullname);
+            options.IdentityResources["openid"].UserClaims.Add(Constants.Claims.LastLogin);
+            options.IdentityResources["openid"].UserClaims.Add(Constants.Claims.Role);
+        })
+        .AddJwtBearerClientAuthentication();
+}
+
+void DependecyInjection()
+{
+    // DI
+    builder.Services.AddScoped<IUserClaimsPrincipalFactory<ApplicationUser>, ApplicationUserClaimsPrincipalFactory>();
+    builder.Services.AddTransient<IRepositoryFactory, RepositoryFactory>();
+}
+
+void AutoMapperConfiguration()
+{
+    var mappingConfig = new MapperConfiguration(mc =>
+    {
+        mc.AddProfile(new MappingProfile());
+    });
+
+    builder.Services.AddSingleton(mappingConfig.CreateMapper());
+}
+
+void SwaggerSetup()
+{
+    builder.Services.AddEndpointsApiExplorer();
+
+    builder.Services.AddSwaggerGen(c =>
+    {
+        c.SwaggerDoc("v1", new OpenApiInfo
+        {
+            Title = "Timinute API",
+            Description = "Docs for Timinute API",
+            Version = "v1",
+            Contact = new OpenApiContact { Email = "jame_581@windowslive.com", Name = "Jan Mesarc" }
+        });
+        c.ResolveConflictingActions(apiDescriptors => apiDescriptors.First());
+    });
+}
