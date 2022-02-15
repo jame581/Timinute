aa71b8f1a39acf15a0004bb5606635507ae0a6c0
Jan Mesarč <jame_581@windowslive.com>
2022-02-15 20:52:15 +0100

    Update OidcConfigurationController.cs

--- Timinute/Server/Controllers/OidcConfigurationController.cs
+++ Timinute/Server/Controllers/OidcConfigurationController.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Timinute.Server.Controllers
 {
-    [AllowAnonymous]
     public class OidcConfigurationController : Controller
     {
         private readonly ILogger<OidcConfigurationController> _logger;
