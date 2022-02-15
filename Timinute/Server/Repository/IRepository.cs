0bed40659775847236066ee9d565206d77d8a3f2
Jan Mesarč <jame_581@windowslive.com>
2022-02-15 18:47:17 +0100

    Base repository extended by paging

--- Timinute/Server/Repository/IRepository.cs
+++ Timinute/Server/Repository/IRepository.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq.Expressions;
+using Timinute.Server.Models.Paging;
 
 namespace Timinute.Server.Repository
 {
@@ -7,6 +8,7 @@ namespace Timinute.Server.Repository
         Task Delete(TEntity entityToDelete);
         Task Delete(object id);
         Task<TEntity?> Find(object id);
+        Task<PagedList<TEntity>> GetPaged(PagingParameters parameters, string includeProperties = "");
         Task<IEnumerable<TEntity>> Get(Expression<Func<TEntity, bool>>? filter = null, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>>? orderBy = null, string includeProperties = "");
         Task<IEnumerable<TType>> Get<TType>(Expression<Func<TEntity, TType>> select, Expression<Func<TEntity, bool>>? where = null) where TType : class;
         Task<TEntity?> GetByIdInclude(Expression<Func<TEntity, bool>>? filter = null, string includeProperties = "");
