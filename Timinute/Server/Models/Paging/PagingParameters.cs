6db9bb07560d3da31c7e9fd5da87aca05e095532
Jan Mesarč <jame_581@windowslive.com>
2022-02-15 16:11:20 +0100

    Paging parameters

---
+++ Timinute/Server/Models/Paging/PagingParameters.cs
@@ -0,0 +1,23 @@
+﻿namespace Timinute.Server.Models.Paging
+{
+    public class PagingParameters
+    {
+        const int maxPageSize = 50;
+        
+        public int PageNumber { get; set; } = 1;
+        
+        private int pageSize = 10;
+        
+        public int PageSize
+        {
+            get
+            {
+                return pageSize;
+            }
+            set
+            {
+                pageSize = (value > maxPageSize) ? maxPageSize : value;
+            }
+        }
+    }
+}
