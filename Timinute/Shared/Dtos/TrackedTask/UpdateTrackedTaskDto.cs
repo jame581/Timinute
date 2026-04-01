6b68d10d1f289f6a144ee8af7a2a8094bd568393
Jan Mesarc <jame_581@windowslive.com>
2026-04-01 11:31:55 +0200

    feat: add validation attributes to TrackedTask DTOs

--- Timinute/Shared/Dtos/TrackedTask/UpdateTrackedTaskDto.cs
+++ Timinute/Shared/Dtos/TrackedTask/UpdateTrackedTaskDto.cs
@@ -1,14 +1,24 @@
-﻿using Timinute.Shared.Dtos.Project;
+using System.ComponentModel.DataAnnotations;
+using Timinute.Shared.Dtos.Project;
 
 namespace Timinute.Shared.Dtos.TrackedTask
 {
     public class UpdateTrackedTaskDto
     {
+        [Required]
         public string TaskId { get; set; } = null!;
+
+        [Required]
+        [StringLength(50, ErrorMessage = "Name of task is too long.", MinimumLength = 2)]
         public string Name { get; set; } = null!;
+
+        [Required]
         public DateTime StartDate { get; set; }
+
         public DateTime? EndDate { get; set; }
+
         public string? ProjectId { get; set; }
+
         public ProjectDto? Project { get; set; }
     }
 }
